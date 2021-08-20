3a76b0f1a81d8fabe3d50c1e24366105c29bd54c
Niklas Göckel <niklasgoeckel@gmail.com>
2021-08-20 20:23:38 +0200

    deployment scripts: added script for scenario 3 (just logging)

---
+++ deployment-scripts/scenario3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+outfile=reports/scenario3.txt
+
+error_msg="Something went wrong, aborted the rest! \
+Check state of cluster, might be problematic!"
+
+echo "----------------SCENARIO 3 STARTING----------------" | tee -a $outfile
+
+./deploytest.sh -r $outfile \
+    -m ../../microservices-demo/kubernetes-manifests/microservices-no-tracing/ \
+    -l ../../microservices-demo/kubernetes-manifests/loadgenerator/loadgenerator100.yaml \
+    -o ../manifests-observability/filebeat.yaml \
+    -o ../manifests-observability/kibana-logging.yaml \
+    -e ../manifests-elasticsearch/elasticsearch-logging.yaml
+
+if [ $? -eq 1 ]; then
+   echo "$error_msg"
+   exit 1
+fi
+
+echo "----------------SCENARIO 3 FINISHED----------------" | tee -a $outfile
