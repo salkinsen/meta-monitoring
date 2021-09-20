5b970c8086460340fe05cc05ec24c7a99e265a75
Niklas Göckel <niklasgoeckel@gmail.com>
2021-09-20 16:47:43 +0200

    removed unnecessary scripts for kind setup and deployment

--- docker-pull-images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# load relevant images into kind
-docker pull docker.elastic.co/elasticsearch/elasticsearch:7.11.2
-docker pull docker.elastic.co/beats/filebeat:7.11.2
-docker pull docker.elastic.co/kibana/kibana:7.11.2
-docker pull docker.elastic.co/beats/metricbeat:7.11.2
-# docker pull quay.io/coreos/kube-state-metrics:2.4.1
--- prepare-kind.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# load relevant images into kind
-kind create cluster --config kind-config.yaml
-./kind-load-images.sh
-kubectl label nodes kind-worker layer=meta-monitoring
-
-# kind load docker-image quay.io/coreos/kube-state-metrics:7.11.2
